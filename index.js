93b19bb116155e9a7a930eb1eae7ddec7beda004
ugonna-onyejekwe <innocentugonna063@gmail.com>
2023-12-10 00:52:36 +0100

    updated

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-let menubar = document.getElementById("menu")
-let closebar = document.getElementById("close")
-let nav  = document.querySelector("nav")
+// let menubar = document.getElementById("menu");
+// let closebar = document.getElementById("close");
+// let nav = document.querySelector("nav");
 
+// menubar.addEventListener("click", showmenu);
+// function showmenu() {
+//   nav.classList.remove("unactive");
+//   nav.classList.add("active");
+// }
 
-menubar.addEventListener("click", showmenu)
-function showmenu() {
-    nav.classList.remove("unactive")
-    nav.classList.add("active")
-}
+// closebar.addEventListener("click", removemenu);
+// function removemenu() {
+//   nav.classList.remove("active");
+//   nav.classList.add("unactive");
+// }
 
-closebar.addEventListener("click", removemenu)
-function removemenu() {
-    nav.classList.remove("active")
-    nav.classList.add("unactive")
-}
+// let link = document.querySelector(".link");
+// for (let i = 0; i < link.length; i++) {
+//   let links = link[i];
+//   links.addEventListener("click", open);
+// }
+// function open(event) {
+//   let openlink = event.target;
+//   openlink.parentElement.classList.add("active");
+// }
 
+const menu_bar = document.querySelector(".menu_btn");
+const nav = document.querySelector("nav");
 
-let link =document.querySelector(".link")
-for(let i=0; i < link.length; i++) {
-    let links = link[i]
-    links.addEventListener("click", open)
-}
- function open(event) {
-let openlink = event.target
-openlink.parentElement.classList.add("active")
- }
-    
- 
+const activate_menu = () => {
+  nav.classList.toggle("active");
+};
+
+menu_bar.onclick = activate_menu;
