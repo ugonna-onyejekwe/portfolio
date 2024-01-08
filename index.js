0e493ef66402a9385b6185cd59fb0046e9cfd772
ugonna-onyejekwe <innocentugonna063@gmail.com>
2024-01-08 10:09:04 -0800

    updated

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
-// let menubar = document.getElementById("menu");
-// let closebar = document.getElementById("close");
-// let nav = document.querySelector("nav");
-
-// menubar.addEventListener("click", showmenu);
-// function showmenu() {
-//   nav.classList.remove("unactive");
-//   nav.classList.add("active");
-// }
-
-// closebar.addEventListener("click", removemenu);
-// function removemenu() {
-//   nav.classList.remove("active");
-//   nav.classList.add("unactive");
-// }
-
-// let link = document.querySelector(".link");
-// for (let i = 0; i < link.length; i++) {
-//   let links = link[i];
-//   links.addEventListener("click", open);
-// }
-// function open(event) {
-//   let openlink = event.target;
-//   openlink.parentElement.classList.add("active");
-// }
-
-const menu_bar = document.querySelector(".menu_btn");
+const menu_btn = document.querySelectorAll(".menu_btn");
 const nav = document.querySelector("nav");
+const anchor = document.querySelectorAll(".links  a");
 
-const activate_menu = () => {
-  nav.classList.toggle("active");
-};
+menu_btn.forEach((i) => {
+  i.onclick = () => {
+    activateMenu();
+  };
+});
 
-menu_bar.onclick = activate_menu;
+anchor.forEach((i) => {
+  console.log(i);
+  i.onclick = () => {
+    activateMenu();
+  };
+});
+
+function activateMenu() {
+  nav.classList.toggle("menu");
+}
